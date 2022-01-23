520c4e3f70e820a9dd87503d84556bca99c9e47e
kudrisss <kudris@bk.ru>
2022-01-23 23:28:27 +0300

    new commit

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ for i in phrase:
         print(f"{i}: {phrase.count(i)}")
         abv += i
 
-# второй способ через "import collections"
-results = collections.Counter(phrase)
+# 2 самые встречающиеся буквы через "import collections"
+
+results = collections.Counter(phrase).most_common(2) 
 print(results)
