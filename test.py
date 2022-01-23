0f304ff18d34d13b0b4c78aa7444208656e75fc9
kudrisss <kudris@bk.ru>
2022-01-23 22:06:01 +0300

    corrections

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import collections
 
+# первый способ
 
 phrase = "Python is the best language ever"
 abv = ""
@@ -7,6 +8,7 @@ for i in phrase:
     if i not in abv:
         print(f"{i}: {phrase.count(i)}")
         abv += i
+# второй способ через "import collections"
 
 results = collections.Counter(phrase) # нашел через "import collections"
 print(results)
