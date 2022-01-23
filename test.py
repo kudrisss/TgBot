667362749d603ccd0771db9a2708c2f0209a1895
kudrisss <kudris@bk.ru>
2022-01-23 23:22:31 +0300

    new comm

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ for i in phrase:
     if i not in abv:
         print(f"{i}: {phrase.count(i)}")
         abv += i
-# второй способ через "import collections"
 
-results = collections.Counter(phrase) # нашел через "import collections"
+# 2 самые встречающиеся буквы через "import collections"
+
+results = collections.Counter(phrase).most_common(2) 
 print(results)
